c60a19e6fc625df4210f37efdecf7cd22b18240b
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-31 01:52:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    page = Nokogiri::HTML(open(index_url))
-    students = []
+   
   end
 
   def self.scrape_profile_page(profile_url)
